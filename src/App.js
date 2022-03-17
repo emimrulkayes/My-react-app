485236e5c9da3c1d1ba1931b92ad0d65b652c0a9
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-03-17 13:19:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const players = ['Sakib Al Hasan', 'Tamim Iqbal', 'Rubel Hossain', 'Musfiqur Rahim', 'Mostafijur', 'Imrul kayes']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>React Componants</h1>
+      {
+        players.map(player => <Player name={player}></Player>)
+      }
+      <Person name="JavaScript" Librery="ReactJs"></Person>
+      <Person name="nodeJs"></Person>
     </div>
   );
+}
+
+
+function Person(props) {
+  // console.log(props);
+  return (
+    <div className="container">
+      <h2>Language: {props.name}</h2>
+      <h3>Library: {props.Librery}</h3>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, ex.</p>
+    </div>
+  )
+}
+
+function Player(props) {
+  // console.log(props);
+  return (
+    <div className="player">
+      <h2>Player Name: {props.name}</h2>
+      <p>Job: Cricketer</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nobis perspiciatis laborum nesciunt aliquam ullam iure ipsa iusto, tempore doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptates, ratione nam sint dicta earum in iste quae debitis eligendi nostrum hic minus itaque. Dolor harum ipsa numquam culpa quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, vitae.</p>
+    </div>
+  )
 }
 
 export default App;
